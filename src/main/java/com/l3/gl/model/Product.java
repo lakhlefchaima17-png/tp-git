3fa46f75a76eab75d314257c628f15a214c8fc79
Rofaida <rofaidalaribi2@gmail.com>
2025-12-29 23:26:11 +0100

    Add Product model class

---
+++ src/main/java/com/l3/gl/model/Product.java
@@ -0,0 +1,14 @@
+package com.l3.gl.model;
+
+public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and setters
+}
